29ae3d6e8c97753330fde46bd48ab065ee3888ad
SeiyaKoga <138374827+seiya718999@users.noreply.github.com>
2023-09-17 09:24:00 +0000

    新規ジャンル作成機能完成

---
+++ app/controllers/admin/genres_controller.rb
@@ -0,0 +1,26 @@
+class Admin::GenresController < ApplicationController
+  
+  def index
+    @genre = Genre.new
+    @genres = Genre.all
+  end
+  
+  def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to edit_admin_genre_path(@genre)
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+  
+  private
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+end

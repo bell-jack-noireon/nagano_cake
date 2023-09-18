5dda794b6f7701ed1a531b569df1c4403ff44524
urunyon <141293154+urunyon@users.noreply.github.com>
2023-09-18 03:42:03 +0000

    管理者topページ

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,2 @@
 class Admin::OrdersController < ApplicationController
-  
-   def top
-    @order = current_customer.orders
-   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,6 @@
 class Order < ApplicationRecord
    belongs_to :customer
    has_many :cart_items
-
+# enum注文ステータスの値
    enum order_status: {入金待ち:0, 入金確認:1, 製作中:2, 発送準備中:3, 発送済:4}
-   
 end

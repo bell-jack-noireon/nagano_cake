c62a7922947126aeeb982f668f67b1c37b849ccd
EC2 Default User <141287848+Shunta126@users.noreply.github.com>
2023-09-18 07:24:57 +0000

    アクティブストレージインストール

---
+++ app/controllers/admin/items_controller.rb
@@ -0,0 +1,31 @@
+class Admin::ItemsController < ApplicationController
+
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      flash[:notice] = "保存されました！"
+    redirect_to admin_item_path(@item.id)
+   else
+     @item = Item.new
+   render :new
+   end
+  end
+
+  def update
+  end
+
+end

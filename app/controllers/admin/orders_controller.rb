c83451673ac7747d4dfe830298deeccc10611339
urunyon <141293154+urunyon@users.noreply.github.com>
2023-09-19 02:06:37 +0000

    [Add]orders_controller

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,6 +5,15 @@ class Admin::OrdersController < ApplicationController
     
   end
   
+  def update
+    @oder = Order.find(params[:id])
+    @oder_details = OrderDetail.where(order_id: params[:id])
+    redirect_to admin_order_detail_path(@order)
+  end
   
+  private
   
+  def order_params
+    params.require(:order).permit(:status)
+  end
 end

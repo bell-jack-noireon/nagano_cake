e70c1c9f5f7c6572bff68616636f50387a04d2eb
EC2 Default User <141287848+Shunta126@users.noreply.github.com>
2023-09-19 04:44:02 +0000

    url記述変更

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -10,11 +10,11 @@ class Admin::ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    if @item.save
+   if @item.save
     redirect_to admin_item_path(@item)
    else
-     @item = Item.new
-   render :new
+    @item = Item.new
+    render :new
    end
   end
 

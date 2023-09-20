63200f739437df17cc8d0f7e7f3a6ce6d2ea517b
EC2 Default User <141287848+Shunta126@users.noreply.github.com>
2023-09-20 04:32:00 +0000

    Revert "finaly admin_items"
    This reverts commit 828caf5d1f7f986917727b3a67526a187658658e.

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -6,7 +6,6 @@ class Admin::ItemsController < ApplicationController
 
   def new
     @item = Item.new
-    @genres = Genre.all
   end
 
   def create
@@ -14,7 +13,7 @@ class Admin::ItemsController < ApplicationController
    if @item.save
     redirect_to admin_item_path(@item)
    else
-    @genres = Genre.all
+    @item = Item.new
     render :new
    end
   end
@@ -25,18 +24,13 @@ class Admin::ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-    @genres = Genre.all
   end
 
 
   def update
     @item = Item.find(params[:id])
-    if @item.update(item_params)
-     redirect_to admin_item_path(@item)
-    else
-     @genres = Genre.all
-     render :edit
-    end
+    @item.update(item_params)
+    redirect_to admin_item_path(@item)
   end
 
   private

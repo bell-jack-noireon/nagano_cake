82ed3d5ca2124f5ac8cf725d724da2a944e933bc
seiya718999 <138374827+seiya718999@users.noreply.github.com>
2023-09-20 17:44:32 +0900

    Update customers_controller.rb

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -30,7 +30,7 @@ class Public::CustomersController < ApplicationController
   
   private
    def customer_params
-     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :emaile, :address, :post_code, :phone_number, :is_deleted)
+     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :emaile, :address, :post_code, :phone_number)
    end
      
 end

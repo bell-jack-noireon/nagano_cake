4ec30c834448eb1abf45772609c8d1508b5c2b2f
urunyon <141293154+urunyon@users.noreply.github.com>
2023-09-21 05:36:49 +0000

    [finish]public-orders-show

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,6 @@
 class Address < ApplicationRecord
   belongs_to :customer
-  
+
   validates :post_code, presence: true, format: {with: /\A\d{7}\z/}
   validates :address, presence: true
   validates :name, presence: true

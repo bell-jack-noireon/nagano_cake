7a6b2efcec58abf0e2db2e17360625fbf9d726eb
EC2 Default User <ec2-user@ip-172-31-16-124.ap-northeast-1.compute.internal>
2023-09-21 08:26:32 +0000

    [Add]validation flashmessage

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,7 +1,8 @@
 class CartItem < ApplicationRecord
    belongs_to :item
    belongs_to :customer
-   
+
+   validates :amount, presence: true
    
    def subtotal
      item.tax_included_price * amount

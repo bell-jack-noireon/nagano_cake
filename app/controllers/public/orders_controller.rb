d9c630ded6669c500b4a2fbc066b226aea3b3bfa
EC2 Default User <141287848+Shunta126@users.noreply.github.com>
2023-09-21 06:16:14 +0000

    finish_address

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,12 +6,34 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
+    @order = Order.new(order_params)
+
+    if params[:order][:address_id] == "0"
+      @order.post_code = current_customer.post_code
+      @order.address = current_customer.address
+      @order.name = current_customer.first_name + current_customer.last_name
+
+    elsif params[:order][:address_id] == "1"
+      @address = Address.find(params[:order][:address_id])
+      @order.post_code = @address.post_code
+      @order.address = @address.address
+      @order.name = @address.name
+
+    else params[:order][:address_id] == "2"
+      @order.post_code = params[:order][:post_code]
+      @order.address = params[:order][:address]
+      @order.name = params[:order][:name]
+    end
+
+  　@cart_items = current_customer.cart_items.all
+
+  end
+
+
+  def create
   end
 
   def completion
-  end
-
-  def create
   end
 
   def index
@@ -23,7 +45,7 @@ class Public::OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit()
+    params.require(:order).permit(:payment_method, :post_cade, :address, :name)
   end
 
 end

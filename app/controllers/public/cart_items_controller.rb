1c03575acfbdb02ef51ad2d4db81cc6d032d8ba0
urunyon <141293154+urunyon@users.noreply.github.com>
2023-09-22 06:04:59 +0000

    [finish]public_cart

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -10,6 +10,47 @@ class Public::CartItemsController < ApplicationController
     if @cart_item_check
       @cart_item_check.amount += params[:cart_item][:amount].to_i
       @cart_item_check.save
-      flash[:success] = "既にカートに入っている商品です"
+      flash[:notice] = "カートに商品を追加しました"
       redirect_to cart_items_path
     else
+      @cart_item = CartItem.new(cart_item_params)
+      @cart_item.customer_id = current_customer.id
+      if @cart_item.save
+        flash[:notice] = "カートに商品を追加しました"
+        redirect_to cart_items_path
+      end
+    end
+  end
+
+  def update
+    @cart_item = CartItem.find(params[:id])
+    if @cart_item.update(cart_item_params)
+      flash[:notice] = "個数を変更しました"
+      redirect_back(fallback_location: root_path)
+    else
+      #flash[:] = "正しい個数(整数)を入力してください"
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  def destroy_all
+    CartItem.where(customer_id: current_customer.id).destroy_all
+    flash[:notice] = "カートの中身を空にしました"
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    flash[:notice] = "選択した商品を削除しました"
+    redirect_back(fallback_location: root_path)
+  end
+
+
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
+  end
+end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,6 +2,7 @@ class CartItem < ApplicationRecord
    belongs_to :item
    belongs_to :customer
    
+   validates :amount, numericality: { only_integer: true 
    
    def subtotal
      item.tax_included_price * amount

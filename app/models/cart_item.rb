a4fe0b43f477553d446e6a682e454d3e6407b776
urunyon <141293154+urunyon@users.noreply.github.com>
2023-09-22 06:07:03 +0000

    [finish]public-cart

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,7 +2,6 @@ class CartItem < ApplicationRecord
    belongs_to :item
    belongs_to :customer
    
-   validates :amount, numericality: { only_integer: true 
    
    def subtotal
      item.tax_included_price * amount

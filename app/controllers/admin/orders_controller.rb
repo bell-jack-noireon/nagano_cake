aedb832f9ef96f2af3f4e1cfe6cf1d28c2820463
EC2 Default User <141287848+Shunta126@users.noreply.github.com>
2023-09-23 08:52:52 +0000

    エラー修正

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,22 +1,22 @@
 class Admin::OrdersController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def show
     @order = Order.find(params[:id])
     @order_details = OrderDetail.where(order_id: params[:id])
   end
-  
+
   def update
     @order = Order.find(params[:id])
     @order_details = OrderDetail.where(order_id: params[:id])
     @order.update(order.params)
     redirect_to admin_order_path(@order)
   end
-  
+
   private
-  
-   def order.params
+
+   def order_params
      params.require(:order).permit(:status)
    end
-   
+
 end

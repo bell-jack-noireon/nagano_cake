d8b28d1c9a62482dc67ed47132c1c49c9041e88a
urunyon <141293154+urunyon@users.noreply.github.com>
2023-09-23 08:15:41 +0000

    [Finish]test-4-

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -32,7 +32,7 @@ class Public::CustomersController < ApplicationController
 
   private
    def customer_params
-     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :emaile, :address, :post_code, :phone_number)
+     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :address, :post_code, :phone_number)
    end
 
 end

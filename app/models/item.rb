997083baabb3c30a2514986068c24c9e5135d0fa
EC2 Default User <141287848+Shunta126@users.noreply.github.com>
2023-09-25 04:16:58 +0000

    finish error

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,10 @@ class Item < ApplicationRecord
   has_many :order_details
   belongs_to :genre
 
+  validates :name, presence: true
+  validates :body, presence: true
+  validates :price, presence: true
+
   def get_item_image(width, height)
     unless item_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.png')
@@ -12,11 +16,11 @@ class Item < ApplicationRecord
     end
     item_image.variant(resize_to_limit: [width, height]).processed
   end
-  
+
   TaxRate = 10
   #税抜価格＋消費税で税込価格
   def tax_incluted_price
     (price * (1 + TaxRate * 0.01)).floor
   end
-  
+
 end

22ae860824d167fa0758c32fda261cbc472ee7e7
SeiyaKoga <138374827+seiya718999@users.noreply.github.com>
2023-09-25 06:30:32 +0000

    [finish]ジャンル検索機能

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,8 +1,14 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.page(params[:page]).per(8)
-    @items_all = Item.all
     @genres = Genre.all
+    if params[:genre_id]
+      @genre = Genre.find(params[:genre_id])
+      @items = @genre.items.page(params[:page]).per(8)
+      @items_all = @genre.items
+    else
+      @items = Item.page(params[:page]).per(8)
+      @items_all = Item.all
+    end
   end
 
   def show
